bc65be6b32550aacff6be482e3e8fa1f6139f6e3
TheLuckyman30 <bnauta0@gmail.com>
2025-11-22 18:00:24 -0500

    Modified main prod deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>This is a test 2</div>
+      <div>This is Main</div>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const BASE_TYPE: number = Number(process.env.BASE_TYPE);
-const PR_NUMBER: number = Number(process.env.PR_NUMBER);
+const BASE_TYPE: number = Number(process.env.BASE_TYPE ?? 0);
+const PR_NUMBER: number = Number(process.env.PR_NUMBER ?? 0);
 const baseUrls = ["/test-devops/", `/test-devops/pr-preview/pr-${PR_NUMBER}`];
 
 // https://vite.dev/config/
