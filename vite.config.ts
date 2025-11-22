ef77e598e11aaa6386bed3cd0b2d40a61c719415
TheLuckyman30 <bnauta0@gmail.com>
2025-11-22 17:43:48 -0500

    Set vite base url based on deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const BASE_TYPE: number = Number(process.env.BASE_TYPE);
+const PR_NUMBER: number = Number(process.env.PR_NUMBER);
+const baseUrls = ["/test-devops/", `/test-devops/pr-preview/pr-${PR_NUMBER}`];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-devops/",
+  base: baseUrls[BASE_TYPE],
 });
